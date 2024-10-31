3967663b149d505bde3dcb9e316391468f4fe165
el-rey08 <hammedlanre71@gmail.com>
2024-10-31 13:57:20 +0100

    my first commit

---
+++ utils/multer.js
@@ -0,0 +1,30 @@
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype.startsWith('image/') || 
+        file.mimetype.startsWith('video/') || 
+        file.mimetype === 'image/gif'
+    ) {
+        cb(null, true);
+    } else {
+        cb(new Error('Only images, videos, and GIFs are allowed.'));
+    }
+};
+
+const upload = multer({
+    storage,
+    fileFilter,
+    limits: { fileSize: 1024 * 1024 * 10 },
+});
+
+module.exports = upload;
