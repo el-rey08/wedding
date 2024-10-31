f7e29a9572164f9e224d2c6a5f79c250f77bf3a7
el-rey08 <hammedlanre71@gmail.com>
2024-10-31 14:25:19 +0100

    user signUp

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const cloudinary = require('../utils/cloudinary')
 
 exports.signUp = async(req, res)=>{
     try {
@@ -16,8 +17,30 @@ exports.signUp = async(req, res)=>{
         }
         const existingUser = await userModel.findOne({email: email.toLowerCase().trim()})
         if(existingUser){
-            return
+            return res.status(400).json({
+                message: 'user already exist'
+            })
         }
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(password, salt)
+        const file = req.file;
+        if (!file) {
+          return res.status(400).json({
+            message: "Student picture is required",
+          });
+        }
+    
+        const image = await cloudinary.uploader.upload(req.file.path);
+        const data = new userModel({
+            userName,
+            email: email.toLowerCase().trim(),
+            password: hash
+        })
+        await data.save()
+        res.status(200).json({
+            message: 'usere created successfully',
+            data
+        })
     } catch (error) {
         res.status(500).json({
             message: error.message
